3549df5abf2c81010902fd6a0c63339460f8f370
Corvin <corvinszimion@googlemail.com>
2025-04-22 22:26:57 +0200

    added repro steps

---
+++ issue3735/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace issue3735;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+///
+public partial class MainWindow : Window
+{
+    public RootDialog RootDialog { get; } = new();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Application.Current.Dispatcher.InvokeAsync(async () =>
+        {
+            await Delay();
+            DialogHost.Show(RootDialog, "MainDialog");
+            await Delay();
+            RootDialog.OpenInnerDialog();
+            await Delay();
+
+            //RootDialog.CloseInnerDialog();
+            DialogHost.Close("MainDialog");
+
+            await Delay();
+            DialogHost.Show(RootDialog, "MainDialog");
+        });
+
+        static Task Delay() => Task.Delay(2000);
+    }
+}
---
+++ issue3735/RootDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace issue3735;
+
+/// <summary>
+/// Interaction logic for RootDialog.xaml
+/// </summary>
+public partial class RootDialog : UserControl
+{
+    public RootDialog()
+    {
+        InitializeComponent();
+    }
+
+    internal void CloseInnerDialog() => DialogHost.Close("ucDialogHost");
+
+    internal void OpenInnerDialog()
+    {
+        var tb = new TextBlock
+        {
+            Text = "Inner dialog content",
+            Margin = new Thickness(16)
+        };
+        DialogHost.Show(tb, "ucDialogHost");
+    }
+}
