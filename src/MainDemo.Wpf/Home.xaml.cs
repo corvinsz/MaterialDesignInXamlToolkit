6c3d4df5bf490161f4533ff1c8309f04b07030c1
Corvin <43533385+corvinsz@users.noreply.github.com>
2026-06-16 21:50:06 +0200

    add repro project

--- src/MainDemo.Wpf/Home.xaml.cs
+++ src/MainDemo.Wpf/Home.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using MaterialDesignDemo.Domain;
+using MaterialDesignThemes.Wpf;
 
 namespace MaterialDesignDemo;
 
@@ -7,8 +8,8 @@ public partial class Home
 {
     public Home() => InitializeComponent();
 
-    private void GitHubButton_OnClick(object sender, RoutedEventArgs e)
-        => Link.OpenInBrowser(ConfigurationManager.AppSettings["GitHub"]);
+    private async void GitHubButton_OnClick(object sender, RoutedEventArgs e)
+        => await DialogHost.Show(new Issue3428(), "RootDialog");
 
     private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
         => Link.OpenInBrowser("https://twitter.com/James_Willock");
---
+++ src/MainDemo.Wpf/Issue3428.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaterialDesignDemo
+{
+    /// <summary>
+    /// Interaction logic for Issue3428.xaml
+    /// </summary>
+    public partial class Issue3428 : UserControl
+    {
+        public Issue3428()
+        {
+            InitializeComponent();
+            //(wfhSample.Child as System.Windows.Forms.WebBrowser).Navigate("http://www.wpf-tutorial.com");
+        }
+    }
+}
