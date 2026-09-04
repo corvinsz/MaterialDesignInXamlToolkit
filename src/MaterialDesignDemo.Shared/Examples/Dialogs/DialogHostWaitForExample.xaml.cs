c5ff7496a207d6127b97f7069f38209487f635c6
Corvin <43533385+corvinsz@users.noreply.github.com>
2026-09-04 22:20:31 +0200

    trying to add DialogHost.WaitFor* examples to demo apps Tried to add DialogHost.WaitFor* examples to demo apps but there  seems to be a problem with ShowMeTheXaml or the demo apps setup

---
+++ src/MaterialDesignDemo.Shared/Examples/Dialogs/DialogHostWaitForExample.xaml.cs
@@ -0,0 +1,49 @@
+﻿using ShowMeTheXAML;
+
+namespace MaterialDesignDemo.Shared.Examples.Dialogs;
+
+/// <summary>
+/// Interaction logic for DialogHostWaitForExample.xaml
+/// </summary>
+public partial class DialogHostWaitForExample : UserControl
+{
+    private DateTime? _sample7OpenClicked;
+    private DateTime? _sample7CloseClicked;
+
+    public DialogHostWaitForExample()
+    {
+        InitializeComponent();
+    }
+
+    private async void Sample7_OpenButton_Click(object sender, RoutedEventArgs e)
+    {
+        _sample7OpenClicked = DateTime.Now;
+        Sample7_OpenClicked.Text = _sample7OpenClicked.Value.TimeOfDay.ToString(@"hh\:mm\:ss\.fff");
+
+        await Sample7DialogHost.WaitForOpened();
+        var openedFinished = DateTime.Now;
+        Sample7_OpenedFinished.Text = openedFinished.TimeOfDay.ToString(@"hh\:mm\:ss\.fff");
+
+        if (_sample7OpenClicked.HasValue)
+        {
+            var diff = openedFinished - _sample7OpenClicked.Value;
+            Sample7_OpenTimeDifference.Text = ((long)diff.TotalMilliseconds).ToString() + " ms";
+        }
+
+        await Sample7DialogHost.WaitForClosed();
+        var closedFinished = DateTime.Now;
+        Sample7_ClosedFinished.Text = closedFinished.TimeOfDay.ToString(@"hh\:mm\:ss\.fff");
+
+        if (_sample7CloseClicked.HasValue)
+        {
+            var closeDiff = closedFinished - _sample7CloseClicked.Value;
+            Sample7_CloseTimeDifference.Text = ((long)closeDiff.TotalMilliseconds).ToString() + " ms";
+        }
+    }
+
+    private void Sample7_CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        _sample7CloseClicked = DateTime.Now;
+        Sample7_CloseClicked.Text = _sample7CloseClicked.Value.TimeOfDay.ToString(@"hh\:mm\:ss\.fff");
+    }
+}
